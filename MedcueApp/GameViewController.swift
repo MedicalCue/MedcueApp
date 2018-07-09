196d78e7f885fb7fa162ad3698c673352c6c3432
Ardella Phoa <ardella.phoa@medicalcue.com>
2018-07-09 16:07:03 -0700

    test2

--- MedcueApp/GameViewController.swift
+++ MedcueApp/GameViewController.swift
@@ -1,4 +1,4 @@
-//
+//test
 //  GameViewController.swift
 //  MedcueApp
 //
--- MedcueApp/Scenarios.swift
+++ MedcueApp/Scenarios.swift
@@ -1,4 +1,4 @@
-//
+//test
 //  Scenarios.swift
 //  MedcueApp
 //
@@ -9,27 +9,13 @@
 import Foundation
 import SpriteKit
 
-    
-    let coun = 5
-    var scenarios: [[Any]] = Array(repeating: Array(repeating: 0, count: coun), count: coun)
-    scenarios[0][0] = "A1"
-    scenarios[0][1] = "50"
-    scenarios[0][2] = "Vaginal"
-    scenarios[0][3] = "4.0"
-    
-    scenarios[1][0] = "B"
-    scenarios[2][0] = "C"
-    scenarios[3][0] = "D"
+
     
     
     
     
     
-    
-    
-    
-    
-}
+
 
 
 
